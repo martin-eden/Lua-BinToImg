4a470cf9f10d2bc522dddbabaac011f42ed923d1
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-08-09 17:36:21 +0200

    [/>] [build] Renamed to "builder/". Now also uses Lua code reformatter.

--- builder/create_readmes_images.sh
+++ builder/create_readmes_images.sh
@@ -4,7 +4,7 @@
 # Create images of Lua and compiled code. They are used in "Readme.md"
 #
 # Author: Martin Eden
-# Last mod.: 2026-06-06
+# Last mod.: 2026-08-09
 #
 
 set -eu
@@ -17,12 +17,12 @@ file_to_png() {
   local tmp_file_png=/tmp/bin_to_png.work.bin.png
 
   cp "$input_file" $tmp_file_bin
-  ../bin/bin_to_png.sh $tmp_file_bin
+  ./bin_to_png.sh $tmp_file_bin
   rm $tmp_file_bin
   mv $tmp_file_png "$output_file"
 }
 
 file_to_png '/usr/local/bin/lua' '../extras/Lua.png'
-file_to_png '../bin/bin_to_ppm' '../extras/bin_to_ppm.png'
+file_to_png '../deploy/bin_to_ppm' '../extras/bin_to_ppm.png'
 
 # 2026-06-06
---
+++ builder/rebuild.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#
+# Create combined Lua file from sources. Create images for Readme.
+#
+# Author: Martin Eden
+# Last mod.: 2026-08-09
+#
+
+#
+# Results are placed in "deploy/"
+#
+# Toolchain uses my "lua code melder" tool to combine files into one:
+#
+#   https://github.com/martin-eden/lua_code_melder
+#
+# Toolchain uses my "lua code formatter" tool to strip comments:
+#
+#   https://github.com/martin-eden/lua_code_formatter
+#
+
+set -e -u
+
+#
+# src/
+#
+
+cd ../src
+
+rm -r -f workshop/
+
+lua ../builder/create_deploy.lua
+
+bash deploy.sh
+rm deploy.sh
+
+mv deploy/workshop/ .
+rm -r -f deploy/
+
+#
+# builder/
+#
+
+cd ../builder
+
+# ( Combine all Lua code, reformat and strip comments
+./meld ../src/ bin_to_ppm > ../deploy/bin_to_ppm.melded.lua
+
+./reformat_lua \
+  ../deploy/bin_to_ppm.melded.lua \
+  ../deploy/bin_to_ppm.melded.stripped.lua \
+  --~keep-comments \
+  --right-margin=72
+rm ../deploy/bin_to_ppm.melded.lua
+
+mv \
+  ../deploy/bin_to_ppm.melded.stripped.lua \
+  ../deploy/bin_to_ppm.lua
+# )
+
+#
+# deploy/
+#
+
+cd ../deploy
+
+# Add shebang to compiled code
+echo '#!/usr/local/bin/lua' > bin_to_ppm.shebanged.lua
+echo >> bin_to_ppm.shebanged.lua
+cat bin_to_ppm.lua >> bin_to_ppm.shebanged.lua
+rm bin_to_ppm.lua
+mv bin_to_ppm.shebanged.lua bin_to_ppm
+
+chmod +x bin_to_ppm
+
+# Create images used in "Readme.md"
+cp ../builder/create_readmes_images.sh .
+./create_readmes_images.sh
+rm create_readmes_images.sh
+
+# 2026-06-01
+# 2026-08-09
